4665b7eaeab42a689bbcf076d5cf0899853b999d
Frederik <masterkooo@gmail.com>
2023-12-09 17:38:57 +0100

    Drag n' Drop

--- app/(platform)/(dashboard)/board/[boardId]/_components/card-item.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/card-item.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { Card } from '@prisma/client'
+import { Draggable } from '@hello-pangea/dnd'
 
 interface CardFormProps {
   data: Card
@@ -9,10 +10,19 @@ interface CardFormProps {
 
 export const CardItem = ({ data, index }: CardFormProps) => {
   return (
-    <div
-      role="button"
-      className="truncate border-2 border-transparent hover:border-black py-2 px-3 text-sm bg-white rounded-md shadow-sm">
-      {data.title}
-    </div>
+    <Draggable
+      draggableId={data.id}
+      index={index}>
+      {(provided) => (
+        <div
+          {...provided.draggableProps}
+          {...provided.dragHandleProps}
+          ref={provided.innerRef}
+          role="button"
+          className="truncate border-2 border-transparent hover:border-black py-2 px-3 text-sm bg-white rounded-md shadow-sm">
+          {data.title}
+        </div>
+      )}
+    </Draggable>
   )
 }
